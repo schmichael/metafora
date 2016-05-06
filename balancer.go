f383261c341ddbdf31b513c6eb62931a1d430674
Michael Schurter <schmichael@lytics.io>
2016-05-05 17:03:48 -0700

    WIP modifying balancer

--- balancer.go
+++ balancer.go
@@ -29,21 +29,16 @@ type BalancerContext interface {
 // clusters are cooperatively balanced -- meaning each node needs to know how
 // to balance itself.
 type Balancer interface {
-	// Init is called once and only once before any other Balancer methods are
-	// called. The context argument is meant to expose functionality that might
-	// be useful for CanClaim and Balance implementations.
-	Init(BalancerContext)
-
 	// CanClaim should return true if the consumer should accept a task.
 	//
 	// When denying a claim by returning false, CanClaim should return the time
 	// at which to reconsider the task for claiming.
-	CanClaim(task Task) (ignoreUntil time.Time, claim bool)
+	CanClaim(ctx BalancerContext, task Task) (ignoreUntil time.Time, claim bool)
 
 	// Balance should return the list of Task IDs that should be released. The
 	// criteria used to determine which tasks should be released is left up to
 	// the implementation.
-	Balance() (release []string)
+	Balance(ctx BalancerContext) (release []string)
 }
 
 // DumbBalancer is the simplest possible balancer implementation which simply
@@ -52,14 +47,11 @@ var DumbBalancer = dumbBalancer{}
 
 type dumbBalancer struct{}
 
-// Init does nothing.
-func (dumbBalancer) Init(BalancerContext) {}
-
 // CanClaim always returns true.
-func (dumbBalancer) CanClaim(Task) (time.Time, bool) { return NoDelay, true }
+func (dumbBalancer) CanClaim(BalancerContext, Task) (time.Time, bool) { return NoDelay, true }
 
 // Balance never returns any tasks to balance.
-func (dumbBalancer) Balance() []string { return nil }
+func (dumbBalancer) Balance(BalancerContext) []string { return nil }
 
 // Provides information about the cluster to be used by FairBalancer
 type ClusterState interface {
@@ -93,35 +85,26 @@ func NewDefaultFairBalancerWithThreshold(nodeid string, cs ClusterState, thresho
 type FairBalancer struct {
 	nodeid string
 
-	bc           BalancerContext
 	clusterstate ClusterState
 
 	releaseThreshold float64
 	delay            time.Time
 }
 
-func (e *FairBalancer) Init(s BalancerContext) {
-	e.bc = s
-}
-
 // CanClaim rejects tasks for a period of time if the last balance released
 // tasks. Otherwise all tasks are accepted.
-func (e *FairBalancer) CanClaim(task Task) (time.Time, bool) {
+func (e *FairBalancer) CanClaim(_ BalancerContext, task Task) (time.Time, bool) {
 	if e.delay.After(time.Now()) {
 		// Return delay set by Balance()
 		return e.delay, false
 	}
-
-	// Sleep proportional to number of tasks
-	n := len(e.bc.Tasks())
-	time.Sleep(time.Duration(n>>2) * time.Millisecond)
 	return NoDelay, true
 }
 
 // Balance releases tasks if this node has 120% more tasks than the average
 // node in the cluster.
-func (e *FairBalancer) Balance() []string {
-	nodetasks := e.bc.Tasks()
+func (e *FairBalancer) Balance(ctx BalancerContext) []string {
+	nodetasks := ctx.Tasks()
 
 	// Reset delay
 	e.delay = time.Time{}
@@ -171,4 +154,82 @@ func (e *FairBalancer) desiredCount(current map[string]int) int {
 	}
 
 	return int(math.Ceil(float64(avg) * e.releaseThreshold))
+}
+
+// BalancerMux allows using multiple balancers as single balancer.
+type BalancerMux struct {
+	bals []Balancer
+}
+
+// NewBalancerMux creates a Balancer which calls each passed in Balancer in
+// order. Read individual methods for behaviors.
+func NewBalancerMux(b ...Balancer) Balancer {
+	return BalancerMux{b}
+}
+
+// CanClaim calls CanClaim on each inner balancer returning the first delayed
+// result reached.
+func (b BalancerMux) CanClaim(ctx BalancerContext, task Task) (time.Time, bool) {
+	for _, innerb := range b.bals {
+		if t, claim := innerb.CanClaim(ctx, task); !claim {
+			return t, claim
+		}
+	}
+	return NoDelay, true
+}
+
+// Balance calls Balance on each inner balancer with a modified
+// BalancerContext. The modified BalancerContext tracks what tasks each Balance
+// call removes so each consecutive Balance call only gets the remaining set of
+// tasks.
+//
+// The union of all returned tasks are then returned.
+func (b BalancerMux) Balance(ctx BalancerContext) []string {
+	mux := newMuxCtx(ctx)
+	for _, innerb := range b.bals {
+		for _, taskid := range innerb.Balance(mux) {
+			mux.add(taskid)
+		}
+	}
+	return mux.result()
+}
+
+type muxctx struct {
+	remaining   []RunningTask
+	releasedset map[string]struct{}
+}
+
+func newMuxCtx(ctx BalancerContext) *muxctx {
+	return &muxctx{
+		remaining:   ctx.Tasks(),
+		releasedset: map[string]struct{}{},
+	}
+}
+
+func (m *muxctx) add(taskid string) {
+	m.releasedset[taskid] = struct{}{}
+
+	// Remove task from remaining task list
+	r := make([]RunningTask, len(m.remaining)-1)
+	i := 0
+	for _, rt := range m.remaining {
+		if rt.ID() != taskid {
+			r[i] = rt
+			i++
+		}
+	}
+}
+
+func (m *muxctx) result() []string {
+	r := make([]string, len(m.releasedset))
+	i := 0
+	for taskid := range m.releasedset {
+		r[i] = taskid
+		i++
+	}
+	return r
+}
+
+func (m *muxctx) Tasks() []RunningTask {
+	return m.remaining
 }
